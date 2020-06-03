4c2b1d6fb5b74cd78be0b0d70612cc10953abf10
Matthew Lazeroff <mdlazeroff@gmail.com>
2020-06-03 16:28:57 -0700

    update required install packages

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ setup(
     package_data={'scram': ['*']},
     include_package_data=True,
     setup_requires=['wheel'],
-    install_requires=['cryptography'],
+    install_requires=['cryptography', 'pytest', 'pytest-mock'],
     entry_points={'console_scripts': 'scram = scram.scrammer:main'},
     tests='tests',
     tests_require=['pytest', 'pytest-mock']
